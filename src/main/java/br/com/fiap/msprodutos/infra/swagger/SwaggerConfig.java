7f79aa1fa525f18edc100e9992662fd426780f89
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-07-04 13:34:55 -0300

    Alterado para expor swagger via docker

--- src/main/java/br/com/fiap/msprodutos/infra/swagger/SwaggerConfig.java
+++ src/main/java/br/com/fiap/msprodutos/infra/swagger/SwaggerConfig.java
@@ -2,18 +2,44 @@ package br.com.fiap.msprodutos.infra.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${spring.profiles.active:Unknown}")
+    String activeProfile;
+
+    @Value("${spring.application.name:Unknown}")
+    String applicationName;
+
+    @Value("${server.port:Unknown}")
+    String serverPort;
+    
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
+    OpenAPI openAPI() {
+        OpenAPI info = new OpenAPI()
                 .info(new Info()
                         .title("APIs do Microserviço de Produtos")
                         .version("v1")
                         .description("APIs do Microserviço de Produtos criada exclusivamente para o TechChallenge 5 da FIAP.")
                 );
+
+        if("docker".equals(activeProfile)){
+            info.servers(Arrays.asList(getServer()));
+        }
+        return info;
+    }
+
+    private Server getServer() {
+        Server server = new Server();
+        server.setDescription("Gateway Docker");
+        server.setUrl("http://"+applicationName+":"+serverPort);
+        return server;
     }
 }
