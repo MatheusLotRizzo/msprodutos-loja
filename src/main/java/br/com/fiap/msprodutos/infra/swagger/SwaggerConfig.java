ded648ab805cebb0bb3a4a5b024bbe09a4b6f47d
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-07-14 22:37:30 -0300

    remoçao server customizado swagger

--- src/main/java/br/com/fiap/msprodutos/infra/swagger/SwaggerConfig.java
+++ src/main/java/br/com/fiap/msprodutos/infra/swagger/SwaggerConfig.java
@@ -2,24 +2,11 @@ package br.com.fiap.msprodutos.infra.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    @Value("${spring.profiles.active:Unknown}")
-    String activeProfile;
-
-    @Value("${spring.application.name:Unknown}")
-    String applicationName;
-
-    @Value("${server.port:Unknown}")
-    String serverPort;
     
     @Bean
     OpenAPI openAPI() {
@@ -29,17 +16,8 @@ public class SwaggerConfig {
                         .version("v1")
                         .description("APIs do Microserviço de Produtos criada exclusivamente para o TechChallenge 5 da FIAP.")
                 );
-
-        if("docker".equals(activeProfile)){
-            info.servers(Arrays.asList(getServer()));
-        }
+       
         return info;
     }
 
-    private Server getServer() {
-        Server server = new Server();
-        server.setDescription("Gateway Docker");
-        server.setUrl("http://"+applicationName+":"+serverPort);
-        return server;
-    }
 }
